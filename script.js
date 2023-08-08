bca5931224bc973adc660f42d3d4d4ae5514721f
Fianu Dovlo <dovlofianu@gmail.com>
2023-08-08 04:07:57 +0000

    Add variables for dom values for buttons

---
+++ script.js
@@ -0,0 +1,9 @@
+const numberButtons = document.getElementsByClassName("js-number");
+const operationButtons = document.getElementsByClassName("js-operation");
+const equalsButton = document.getElementById("js-equals");
+const deleteButton = document.getElementById("js-delete");
+const allClearButton = document.getElementById("js-all-clear");
+const previousOperandTextElement = document.getElementById("js-prev-operand");
+const currentOperandTextElement = document.getElementById("js-curr-operand");
+
+// console.log(typeof(numberButtons));
