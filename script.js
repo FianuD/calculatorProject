c6192b03f6a532e1de5a2be2f505181f2658f191
Fianu Dovlo <dovlofianu@gmail.com>
2023-08-08 04:10:42 +0000

    Created class to store information for currently typed numbers for calculations

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+class Calculator {
+    constructor(previousOperandTextElement, currentOperandTextElement) {
+        this.previousOperandTextElement = previousOperandTextElement;
+        this.currentOperandTextElement = currentOperandTextElement;
+    }
+}
+
 const numberButtons = document.getElementsByClassName("js-number");
 const operationButtons = document.getElementsByClassName("js-operation");
 const equalsButton = document.getElementById("js-equals");
@@ -5,5 +12,6 @@ const deleteButton = document.getElementById("js-delete");
 const allClearButton = document.getElementById("js-all-clear");
 const previousOperandTextElement = document.getElementById("js-prev-operand");
 const currentOperandTextElement = document.getElementById("js-curr-operand");
+
 
 // console.log(typeof(numberButtons));
