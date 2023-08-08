c5d8f1ce1b1ce31476f6ff1a56a47a7689b4ddf1
Fianu Dovlo <dovlofianu@gmail.com>
2023-08-08 06:19:05 +0000

    helper function to add commas and parse decimal places properly

--- script.js
+++ script.js
@@ -65,11 +65,33 @@ class Calculator {
         this.operation = undefined;
         this.previousOperand = "";
     }
+
+    // Create helper function to add commas to large numbers
+    getDisplayNumber(number) {
+        const stringNumber = number.toString();
+        const integerNumbers = parseFloat(stringNumber.split(".")[0]);
+        const decimalNumbers = stringNumber.split(".")[1];
+        let integerDisplay;
+        if (isNaN(integerNumbers)) {
+            integerDisplay = "";
+        } else {
+            integerDisplay = integerNumbers.toLocaleString("en", {maximumFractionDigits: 0})
+        }
+        if (decimalNumbers != null) {
+            return `${integerDisplay}.${decimalNumbers}`;
+        } else {
+            return integerDisplay;
+        }
+    }
+
+
     // function to update display of calculator 
     updateDisplay() {
-        this.currentOperandTextElement.innerText = this.currentOperand;
+        this.currentOperandTextElement.innerText = this.getDisplayNumber(this.currentOperand);
         if (this.operation != null ) {
-            this.previousOperandTextElement.innerText = `${this.previousOperand} ${this.operation}`;
+            this.previousOperandTextElement.innerText = `${this.getDisplayNumber(this.previousOperand)} ${this.operation}`;
+        } else {
+            this.previousOperandTextElement.innerText = "";
         }
         
     }
